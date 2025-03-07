9bc1af8c73b428e46cff63c02fe0a6e91fa2d294
CybXII <luft.luft86@googlemail.com>
2025-03-08 00:28:26 +0100

    adjusted Portfolio

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -15,20 +15,28 @@ export class ProjectdataService {
             githubPath:"https://github.com/CybXII/videoflix-backend"
         },
         {
-            name: 'Coderr [Backend]',
-            imgPath: 'assets/img/coderr.png',
-            animation: 'data-aos="fade-left"',
-            skills: 'Python | Django | Django Rest Framework | PostgreSQL',
-            projectPath:"https://coderr.luft-alexander.de/",
-            githubPath:"https://github.com/CybXII/coderr-backend"
-        },
-        {
             name: 'DaBubble [Frontend]',
             imgPath: 'assets/img/dabubble.png',
             animation: 'data-aos="fade-right"',
             skills: 'Angular | TypeScript | Firebase | SCSS  ',
             projectPath:"https://dabubble.luft-alexander.de",
             githubPath:"https://github.com/CybXII/DaBubble"
+        },
+        {
+            name: 'Join [Frontend]',
+            imgPath: 'assets/img/join.png',
+            animation: 'data-aos="fade-right"',
+            skills: 'JavaScript | Firebase | CSS',
+            projectPath:"https://join.luft-alexander.de/",
+            githubPath:"https://github.com/CybXII/JOIN"
+        },
+        {
+            name: 'Coderr [Backend]',
+            imgPath: 'assets/img/coderr.png',
+            animation: 'data-aos="fade-left"',
+            skills: 'Python | Django | Django Rest Framework | PostgreSQL',
+            projectPath:"https://coderr.luft-alexander.de/",
+            githubPath:"https://github.com/CybXII/coderr-backend"
         },
     ];
     public skillsData = [
@@ -38,80 +46,44 @@ export class ProjectdataService {
                     imgPath: 'assets/img/js.svg'
                 },
                 {
-                    name: 'TypeScript',
-                    imgPath: 'assets/img/ts.svg'
+                    name: 'Angular',
+                    imgPath: 'assets/img/angular.svg'
                 },
                 {
-                    name: 'Python',
-                    imgPath: 'assets/img/python.svg'
-                },
-                {   
-                    name: 'SASS/SCSS',
-                    imgPath: 'assets/img/sass.svg'
-                },
-                {
-                    name:'LUA',
-                    imgPath: 'assets/img/lua.svg'
-                },
-                {
-                    name: 'HTML',
-                    imgPath: 'assets/img/html.svg'
+                    name: 'GIT',
+                    imgPath: 'assets/img/git.svg'
                 }
             ]},
             {stack:[
                 {
-                    name: 'Angular',
-                    imgPath: 'assets/img/angular.svg'
+                    name: 'TypeScript',
+                    imgPath: 'assets/img/ts.svg'
                 },
                 {
                     name: 'Django',
                     imgPath: 'assets/img/django.svg'
                 },
                 {
-                    name: 'DRF',
-                    imgPath: 'assets/img/drf.svg'
-                },
-                {
                     name: 'Material design',
                     imgPath: 'assets/img/material.svg'
                 },
-                {
-                    name: 'Bootstrap',
-                    imgPath: 'assets/img/bootstrap.svg'
-                }
             ]},
             {stack:[
+                {
+                    name: 'Python',
+                    imgPath: 'assets/img/python.svg'
+                },
                 {
                     name: "Linux",
                     imgPath: 'assets/img/linux.svg'
                 },
                 {
-                    name: 'GIT',
-                    imgPath: 'assets/img/git.svg'
-                },
-                {
                     name: 'Github',
                     imgPath: 'assets/img/github.svg'
                 },
-                {
-                    name: 'Gitlab',
-                    imgPath: 'assets/img/gitlab.svg'
-                },
-                {
-                    name: 'Postman',
-                    imgPath: 'assets/img/postman.svg'
-                }
                 
             ]},
             {stack:[
-                {
-                    name: 'PostgreSQL',
-                    imgPath: 'assets/img/postgresql.svg'
-                },
-                {
-                    name: 'SQLite',
-                    imgPath: 'assets/img/sqlite.svg'
-                },
                 {
                     name: 'Firebase',
                     imgPath: 'assets/img/firebase.svg'
@@ -121,12 +93,8 @@ export class ProjectdataService {
                     imgPath: 'assets/img/restapis.svg'
                 },
                 {
-                    name: 'Scrum',
-                    imgPath: 'assets/img/scrum.svg'
-                },
-                {   
-                    name: 'CSS',
-                    imgPath: 'assets/img/css.svg'
+                    name: 'PostgreSQL',
+                    imgPath: 'assets/img/postgresql.svg'
                 },
             ]},
         ];
